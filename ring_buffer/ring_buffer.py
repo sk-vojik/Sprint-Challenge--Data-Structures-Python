a756a9f92bc91bda9ca827a0b20115835389683a
sk-vojik <scott.vojik@gmail.com>
2019-05-10 08:44:25 -0700

    ring buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    #check to see if current amount is within capacity. if it is, append, and up current amount
+    if self.current < self.capacity - 1:
+      self.current += 1
+    #if we're at capacity, reset current to 0, and next appended value will override oldest item
+    else:
+      self.current = 0
 
   def get(self):
-    pass
+    return [item for item in self.storage if item != None]
+ 
